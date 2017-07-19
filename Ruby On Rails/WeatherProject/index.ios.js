0b878d94248a9d4a4402fe58a45a092c74bf8eba
likesoomti <soomti@naver.com>
2017-07-19 10:04:37 +0900

    add KNU news

--- Ruby On Rails/WeatherProject/index.ios.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-import {AppRegistry} from 'react-native';
-import WeatherProject from './WeatherProject';
-
-AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
