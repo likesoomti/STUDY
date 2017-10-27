8f0a115282d41c247b80e9ec1774b10f1ffef691
likesoomti <soomti@likelion.org>
2017-10-27 18:42:01 +0900

    android remove

--- Android/HANPlace/app/src/main/java/com/onthecoffee/hanplace/FRAGMENT/map_fragment_fac.java
+++
@@ -1,8 +0,0 @@
-package com.onthecoffee.hanplace.FRAGMENT;
-
-/**
- * Created by Soomti on 2017. 4. 30..
- */
-
-public class map_fragment_fac {
-}
--- Android/HANPlace/app/src/main/java/com/onthecoffee/hanplace/FRAGMENT/map_loo.java
+++
@@ -1,8 +0,0 @@
-package com.onthecoffee.hanplace.FRAGMENT;
-
-/**
- * Created by Soomti on 2017. 5. 29..
- */
-
-public class map_loo {
-}
